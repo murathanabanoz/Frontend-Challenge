52d9142b359093f7aa337b5d7a7072795b056fc3
Murathan Abanoz <126447496+Murathan722@users.noreply.github.com>
2024-07-10 18:48:47 +0300

    Header güncellendi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/header.css";
 
 function Header() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [language, setLanguage] = useState("eng");
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+
+    if (!darkMode) {
+      document.body.classList.add("dark-theme");
+    } else {
+      document.body.classList.remove("dark-theme");
+    }
+  };
+
+  const switchLanguage = () => {
+    setLanguage(language === "eng" ? "tr" : "eng");
+  };
+
   return (
-    <header className="header">
+    <header className={`header ${darkMode ? "dark-theme" : ""}`}>
       <div className="header-top">
         <span className="header-text">
-          <span className="header-text">Dark Mode</span>
+          <span className="dark-mode" onClick={toggleDarkMode}>
+            {darkMode ? "Light Mode" : "Dark Mode"}
+          </span>
           <span className="header-text-ara">|</span>
-          <span className="turkce">Türkçe</span>'ye Geç
+          <span className="turkce" onClick={switchLanguage}>
+            {language === "eng" ? "Türkçe'ye Geç" : "Switch to Eng"}
+          </span>
         </span>
       </div>
       <div className="header-bottom">
