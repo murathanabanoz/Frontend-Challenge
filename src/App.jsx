5749477810407f3c1a976783c3234cf5811deca1
Murathan Abanoz <126447496+Murathan722@users.noreply.github.com>
2024-07-11 18:30:27 +0300

    resetlendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import About from "./components/About.jsx";
 import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <>
       <Header />
-      <About />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,10 @@
-import React, { useState } from "react";
 import "../css/header.css";
 
 function Header() {
-  const [darkMode, setDarkMode] = useState(false);
-  const [language, setLanguage] = useState("eng");
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-
-    if (!darkMode) {
-      document.body.classList.add("dark-theme");
-    } else {
-      document.body.classList.remove("dark-theme");
-    }
-  };
-
-  const switchLanguage = () => {
-    setLanguage(language === "eng" ? "tr" : "eng");
-  };
-
   return (
-    <header className={`header ${darkMode ? "dark-theme" : ""}`}>
-      <div className="header-top">
-        <span className="header-text">
-          <span className="dark-mode" onClick={toggleDarkMode}>
-            {darkMode ? "Light Mode" : "Dark Mode"}
-          </span>
-          <span className="header-text-ara">|</span>
-          <span className="turkce" onClick={switchLanguage}>
-            {language === "eng" ? "Türkçe'ye Geç" : "Switch to Eng"}
-          </span>
-        </span>
-      </div>
-      <div className="header-bottom">
-        <span className="header-link">Skills</span>
-        <span className="header-link">Projects</span>
-        <span className="header-link">Hire Me</span>
-      </div>
-    </header>
+    <>
+      <div className="header">heyyy</div>
+    </>
   );
 }
 
