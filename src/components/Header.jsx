4d3415c0b480dd4bbff74595939984414b479257
Murathan Abanoz <126447496+Murathan722@users.noreply.github.com>
2024-07-12 01:32:57 +0300

    header uptated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
+import React from "react";
 import "../css/header.css";
 
-function Header() {
+const Header = () => {
   return (
-    <>
-      <div className="header">heyyy</div>
-    </>
+    <div className="header">
+      <div className="header-top">
+        <span className="dark-mode-span">Dark Mode</span>
+        <span className="divider">|</span>
+        <span>
+          <span style={{ color: "blue", marginLeft: "-10px" }}>Türkçe</span> 'ye
+          Geç
+        </span>
+      </div>
+      <div className="header-bottom">
+        <button className="skills-btn">Skills</button>
+        <button className="projects-btn">Projects</button>
+        <button className="hire-me-btn">Hire Me</button>
+      </div>
+    </div>
   );
-}
+};
 
 export default Header;
