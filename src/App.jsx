f92b52abf7ba0133f4faf54bc0bce9e428166aaa
Murathan Abanoz <126447496+Murathan722@users.noreply.github.com>
2024-07-18 09:21:49 +0300

    Toastify kullanıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import "./App.css";
 import About from "./components/About.jsx";
 import Footer from "./components/Footer.jsx";
@@ -5,8 +6,26 @@ import Header from "./components/Header.jsx";
 import Profile from "./components/Profile.jsx";
 import Projects from "./components/Projects.jsx";
 import Skills from "./components/Skills.jsx";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  useEffect(() => {
+    toast("Welcome to my website", {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+    });
+  }, []);
+
   return (
     <>
       <Header />
@@ -15,6 +34,7 @@ function App() {
       <Profile />
       <Projects />
       <Footer />
+      <ToastContainer />
     </>
   );
 }
