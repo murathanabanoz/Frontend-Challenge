da1291b789e0ca6b9518dff006e7ab24d14ebe0f
Murathan Abanoz <126447496+Murathan722@users.noreply.github.com>
2025-08-16 23:48:47 +0300

    Projeler güncellendi

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,34 +2,35 @@ import React from "react";
 import { useSelector } from "react-redux";
 import "../css/projects.css";
 import ProjectItem from "./ProjectItem";
+import eCommerce from "../assets/e-commerce.png";
 
 const projectsData = [
   {
     image:
-      "https://img.ekonomim.com/storage/files/images/2024/07/09/netflix-uyelik-ucreti-2024-bir-zam-daha-geldi-netflix-aylik-ucret-ne-kadar-ic29_cover.jpg",
-    title: "Netflix",
+      "https://www.shutterstock.com/image-vector/do-list-planning-icon-concept-600nw-739093225.jpg",
+    title: "To Do List",
     description:
-      "I developed a streaming platform prototype inspired by Netflix, offering a seamless user experience with a vast library of movies and TV shows. Utilizing React.js for front-end development and Node.js for backend, the platform features responsive design, user authentication, and dynamic content delivery.",
+      "I built a To-Do List application with HTML, CSS, and JavaScript, focusing on simplicity and usability. The application allows users to add, delete, and mark tasks as completed, ensuring effective task tracking. With a responsive design, the project delivers a seamless experience across different devices while highlighting my skills in DOM manipulation, event handling, and front-end development fundamentals.",
     descriptionTr:
-      "Netflix'ten ilham alarak geliştirdiğim bir yayın platformu prototipi. Geniş bir film ve TV şovu kütüphanesi sunarak sorunsuz bir kullanıcı deneyimi sağlar. Front-end geliştirmesi için React.js ve backend için Node.js kullandım. Platform, duyarlı tasarım, kullanıcı kimlik doğrulama ve dinamik içerik sunumu özelliklerine sahiptir.",
-    githubLink: "https://github.com/Murathan722/NetFlix-Projesi",
-    viewLink: "https://net-flix-projesi.vercel.app/",
+      "Basitlik ve kullanılabilirliğe odaklanarak HTML, CSS ve JavaScript ile bir To-Do List uygulaması geliştirdim. Uygulama, kullanıcıların görev ekleme, silme ve tamamlanan görevleri işaretleme işlemlerini yapabilmesine imkân tanır. Duyarlı tasarımı sayesinde farklı cihazlarda sorunsuz çalışır. Bu proje, DOM manipülasyonu, event handling (olay yönetimi) ve temel frontend geliştirme becerilerimi ortaya koymaktadır.",
+    githubLink: "https://github.com/murathanabanoz/To-Do-List--JavaScript-",
+    viewLink: "https://to-do-list-java-script-ten.vercel.app/",
   },
   {
     image:
-      "https://www.medyakoridoru.com/wp-content/uploads/2021/09/02/02092021092345_4324763_9_23.jpg",
-    title: "Twitter",
+      "https://media.istockphoto.com/id/1471521270/tr/foto%C4%9Fraf/a-female-hand-and-a-shopping-cart-icon-concept-of-starting-a-sale-online-shopping.jpg?s=612x612&w=0&k=20&c=BWAeFsqceMozUyXcjhj_nf_7SWggnSLTIn25w1nkvog=",
+    title: "E-Ticaret",
     description:
-      "A Twitter-inspired social media platform prototype. It allows users to create and share tweets, follow other users, and engage with content through likes and comments. Built using React.js for the front-end and Node.js for the back-end, the platform features real-time updates, user authentication, and a responsive design.",
+      "I developed an e-commerce project using React.js. The application includes core features such as product listing, filtering, adding items to the cart, and removing them. By leveraging a component-based architecture, I improved code readability and ensured reusability. I used React Hooks for state management, and with a responsive design, the application runs smoothly on both desktop and mobile devices.",
     descriptionTr:
-      "Twitter'dan ilham alarak geliştirdiğim bir sosyal medya platformu prototipi. Kullanıcıların tweet oluşturup paylaşmalarına, diğer kullanıcıları takip etmelerine ve beğeni ve yorumlar aracılığıyla içeriklerle etkileşimde bulunmalarına olanak tanır. Front-end için React.js ve back-end için Node.js kullanılarak oluşturulan platform, gerçek zamanlı güncellemeler, kullanıcı kimlik doğrulama ve duyarlı tasarım özelliklerine sahiptir.",
-    githubLink: "https://github.com/Murathan722/Twitter-Projesi",
-    viewLink: "https://twitter-projesi.vercel.app/",
+      "React.js ile bir e-ticaret projesi geliştirdim. Projede ürünlerin listelenmesi, filtrelenmesi, sepete eklenmesi ve silinmesi gibi temel işlevler yer alıyor. Component tabanlı yapı sayesinde kodun okunabilirliğini artırdım ve yeniden kullanılabilirliği sağladım. State yönetimi için React Hooks kullandım. Ayrıca responsive tasarım sayesinde uygulama hem masaüstü hem de mobil cihazlarda sorunsuz bir şekilde çalışabiliyor.",
+    githubLink: "https://github.com/murathanabanoz/e-ticaret-project",
+    viewLink: "https://e-ticaret-project.vercel.app/",
   },
   {
     image:
       "https://d17wu0fn6x6rgz.cloudfront.net/img/w/tarif/mgt/pizza_14_11zon.webp",
-    title: "Pizza",
+    title: "Pizza Sipariş Uygulaması",
     description:
       "A dynamic pizza ordering application that allows users to customize their pizzas by selecting from various toppings, sizes, and crust types. The project features an intuitive user interface, real-time order updates, and a secure payment system. Built using React.js for the front-end and Node.js with Express for the back-end, the application ensures a seamless and efficient ordering experience.",
     descriptionTr:
